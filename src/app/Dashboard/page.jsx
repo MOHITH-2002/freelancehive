a246772cc9d000f8867f0aa09e4baf1746c042a5
MOHITH-2002 <mohithba9353@gmail.com>
2023-08-31 22:33:30 +0530

    dashboard commit

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import useSWR from 'swr';
 import { MdDelete } from 'react-icons/md';
 import Image from 'next/image';
@@ -83,4 +83,4 @@ const Dashboard = () => {
     }
 }
 
-export default dashboard
+export default Dashboard
