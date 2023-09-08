382082599821249f5e60809219bbda762c444d67
Mohith B A <mohithba9353@gmail.com>
2023-09-08 22:01:58 +0530

    Update page.jsx

--- src/app/Blog/page.jsx
+++ src/app/Blog/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import {notFound} from 'next/navigation'
 export const dynamic = 'force-dynamic'
 const getData = async () => {
-  const res = await fetch('http://localhost:3000/api/posts',{
+  const res = await fetch(`https://freelancehive.vercel.app/api/posts`,{
   cache: "no-store",
 })
   
